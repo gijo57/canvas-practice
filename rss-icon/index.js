a85525ea007c78e0a9b94c799ab261e8b7c90515
Pekka Tiitinen <pekka-1992@hotmail.com>
2021-07-01 13:24:01 +0200

    start last piece of logo

--- rss-icon/index.js
+++ rss-icon/index.js
@@ -7,7 +7,7 @@ const draw = () => {
   ctx.beginPath();
   ctx.arc(100, 190, 20, 0, 2 * Math.PI);
   ctx.fill();
-  ctx.moveTo(80, 143);
+  ctx.moveTo(80, 142);
   ctx.lineTo(80, 110);
   ctx.stroke();
   ctx.arcTo(195, 105, 180, 220, 95);
@@ -16,6 +16,13 @@ const draw = () => {
   ctx.stroke();
   ctx.arcTo(145, 132, 95, 140, 58);
   ctx.fill();
+  ctx.moveTo(80, 85);
+  ctx.lineTo(80, 53);
+  ctx.stroke();
+  ctx.arcTo(255, 53, 245, 220, 155);
+  ctx.stroke();
+  ctx.lineTo(210, 217);
+  ctx.stroke();
 };
 
 draw();
